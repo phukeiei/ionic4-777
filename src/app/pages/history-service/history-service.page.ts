d024646e64942d0468d2528f918350747cf20c57
60160113 <60160113@go.buu.ac.th>
2020-01-13 21:27:02 +0700

    history service

--- src/app/pages/history-service/history-service.page.ts
+++ src/app/pages/history-service/history-service.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HistoryServiceService } from "../../services/history-service/history-service.service";
+import { SessionService } from "../../services/session/session.service";
 @Component({
   selector: 'app-history-service',
   templateUrl: './history-service.page.html',
@@ -7,21 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryServicePage implements OnInit {
   searchTerm: string;
-  datetimeList: any[] = [
-    { date: "10/10/2563", time: "10:10:10" },
-    { date: "11/10/2563", time: "10:20:10" },
-    { date: "12/10/2563", time: "10:30:10" },
-    { date: "13/10/2563", time: "10:40:10" },
-    { date: "14/10/2563", time: "10:50:10" },
-    { date: "15/10/2563", time: "10:60:10" },
-    { date: "16/10/2563", time: "10:70:10" },
-    { date: "17/10/2563", time: "10:80:10" }
+  datetimeList: any = [];
 
-  ];
-
-  constructor() {
+  constructor(
+              public historyServiceService: HistoryServiceService,
+              public sessionService: SessionService
+             ) {
   }
-
+  ngOnInit() {
+    this.getDataByPsId();
+  }
+  getDataByPsId() {
+    this.historyServiceService.getDataByPsId(this.sessionService.userId).subscribe(result => {
+      this.datetimeList = result;
+      console.log(this.datetimeList);
+    });
+  }
   searchChanged() {
     this.datetimeList = this.filterItems(this.searchTerm);
   }
@@ -29,8 +31,6 @@ export class HistoryServicePage implements OnInit {
     return this.datetimeList.filter((item) => {
       return item.TypeOfNotification.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     });
-  }
-  ngOnInit() {
   }
 
 }
--- src/app/services/history-service/history-service.service.ts
+++ src/app/services/history-service/history-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryServiceService {
+  url = 'http://localhost:3000/attend';
+  apiKey = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getDataByPsId(id=null) {
+    return this.http.get(`${this.url}/getDataByPsId/${id}`);
+  }
 }
