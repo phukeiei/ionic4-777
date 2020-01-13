db4022cd166dfcbe36e2a93caba7771b79623b6f
phukeiei <34035927+phukeiei@users.noreply.github.com>
2020-01-13 20:05:31 +0700

    f

--- src/app/pages/confirm-register/confirm-register.page.ts
+++ src/app/pages/confirm-register/confirm-register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+// import { MovieService, SearchType } from './../../services/movie.service';
 
 
 @Component({
@@ -9,14 +11,24 @@ import { ActionSheetController } from '@ionic/angular';
 })
 export class ConfirmRegisterPage implements OnInit {
 
+  results: Observable<any>;
+  searchTerm: string = '';
+  // type: SearchType = SearchType.all;
+
   constructor(public actionSheetController: ActionSheetController) { }
   subscriberList: any[] = [
-    {date: "07/01/2563", id: 60160330,prefix: "นาย", firstname: "กัณณภัต", lastname:"เพียรแก้ว"},
-    {date: "28/12/2562", id: 60160338,prefix: "นาง", firstname: "สาธิมาภรณ์", lastname:"นิรมิตวสุ"},
-    {date: "28/12/2562", id: 60160111,prefix: "นาย", firstname: "พัสกร", lastname:"ใชยะ"}
+    { date: "07/01/2563", id: 60160330, prefix: "นาย", firstname: "กัณณภัต", lastname: "เพียรแก้ว" },
+    { date: "28/12/2562", id: 60160338, prefix: "นาง", firstname: "สาธิมาภรณ์", lastname: "นิรมิตวสุ" },
+    { date: "28/12/2562", id: 60160111, prefix: "นาย", firstname: "พัสกร", lastname: "ใชยะ" }
 
   ];
+
   ngOnInit() {
+  }
+
+  searchChanged() {
+    // Call our service function which returns an Observable
+    // this.results = this.ConfirmRegisterService.searchData(this.searchTerm, this.type);
   }
 
   async presentActionSheet() {
--- src/app/pages/history-service/history-service.page.ts
+++ src/app/pages/history-service/history-service.page.ts
@@ -6,20 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-service.page.scss'],
 })
 export class HistoryServicePage implements OnInit {
-
+  searchTerm: string;
   datetimeList: any[] = [
-    {date: "10/10/2563",time: "10:10:10"},
-    {date: "11/10/2563",time: "10:20:10"},
-    {date: "12/10/2563",time: "10:30:10"},
-    {date: "13/10/2563",time: "10:40:10"},
-    {date: "14/10/2563",time: "10:50:10"},
-    {date: "15/10/2563",time: "10:60:10"},
-    {date: "16/10/2563",time: "10:70:10"},
-    {date: "17/10/2563",time: "10:80:10"}
+    { date: "10/10/2563", time: "10:10:10" },
+    { date: "11/10/2563", time: "10:20:10" },
+    { date: "12/10/2563", time: "10:30:10" },
+    { date: "13/10/2563", time: "10:40:10" },
+    { date: "14/10/2563", time: "10:50:10" },
+    { date: "15/10/2563", time: "10:60:10" },
+    { date: "16/10/2563", time: "10:70:10" },
+    { date: "17/10/2563", time: "10:80:10" }
 
   ];
 
   constructor() {
+  }
+
+  searchChanged() {
+    this.datetimeList = this.filterItems(this.searchTerm);
+  }
+  filterItems(searchTerm) {
+    return this.datetimeList.filter((item) => {
+      return item.TypeOfNotification.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
   }
   ngOnInit() {
   }
