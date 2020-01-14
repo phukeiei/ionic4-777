b6ca781cc4f6491b5a5fdfd4950d7a69e469dad0
60160113 <60160113@go.buu.ac.th>
2020-01-14 13:04:30 +0700

    delete session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,80 +27,56 @@ export class AppComponent implements OnInit {
     this.initializeApp();
   }
   ngOnInit() {
-    if (this.sessionService.status == "") { this.sessionService.status = "1"; }
-    if (this.sessionService.status == "1") {
-      this.appPages = [
-        {
-          title: 'Home',
-          url: '/home-results',
-          direct: 'root',
-          icon: 'home'
-        },
-        {
-          title: 'About',
-          url: '/about',
-          direct: 'forward',
-          icon: 'information-circle-outline'
-        },
-        {
-          title: 'Register',
-          url: '/register-user',
-          direct: 'forward',
-          icon: 'md-person-add'
-        },
-        {
-          title: 'History',
-          url: '/history-service',
-          direct: 'forward',
-          icon: 'document'
-        },
-        {
-          title: 'Edit profile',
-          url: '/edit-profile-swm',
-          direct: 'forward',
-          icon: 'md-hammer'
-        },
-        {
-          title: 'App Settings',
-          url: '/settings',
-          direct: 'forward',
-          icon: 'cog'
-        }
-      ];
-    } else {
-      this.appPages = [
-        {
-          title: 'Home',
-          url: '/home-results',
-          direct: 'root',
-          icon: 'home'
-        },
-        {
-          title: 'About',
-          url: '/about',
-          direct: 'forward',
-          icon: 'information-circle-outline'
-        },
-        {
-          title: 'App Settings',
-          url: '/settings',
-          direct: 'forward',
-          icon: 'cog'
-        },
-        {
-          title: 'Confirm Register',
-          url: '/confirm-register',
-          direct: 'forward',
-          icon: 'person-add'
-        },
-        {
-          title: 'Set service fees',
-          url: '/set-service-fees',
-          direct: 'forward',
-          icon: 'book'
-        }
-      ];
-    }
+    this.appPages = [
+      {
+        title: 'Home',
+        url: '/home-results',
+        direct: 'root',
+        icon: 'home'
+      },
+      {
+        title: 'About',
+        url: '/about',
+        direct: 'forward',
+        icon: 'information-circle-outline'
+      },
+      {
+        title: 'Register',
+        url: '/register-user',
+        direct: 'forward',
+        icon: 'md-person-add'
+      },
+      {
+        title: 'History',
+        url: '/history-service',
+        direct: 'forward',
+        icon: 'document'
+      },
+      {
+        title: 'Edit profile',
+        url: '/edit-profile-swm',
+        direct: 'forward',
+        icon: 'md-hammer'
+      },
+      {
+        title: 'Confirm Register',
+        url: '/confirm-register',
+        direct: 'forward',
+        icon: 'person-add'
+      },
+      {
+        title: 'Set service fees',
+        url: '/set-service-fees',
+        direct: 'forward',
+        icon: 'book'
+      },
+      {
+        title: 'App Settings',
+        url: '/settings',
+        direct: 'forward',
+        icon: 'cog'
+      }
+    ];
   }
   initializeApp() {
     this.platform.ready().then(() => {
